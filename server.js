f6c6bbfdfbc533464306237ebd9da58a91fca407
Christina Larsen <clarsen1782@outlook.com>
2023-10-30 17:02:35 -0700

    Beginning Set Up

---
+++ server.js
@@ -0,0 +1,23 @@
+const inquirer = require('inquirer');
+const mysql = require("mysql");
+
+
+// Create a MySQL connection
+
+// connect to database
+
+// function to start SQP app
+
+// function to view all departments
+
+// function to view all roles
+
+// function to view all employees
+
+// function to add department
+
+// function to add a role
+
+// function to add an employee
+
+// function to update employee role
